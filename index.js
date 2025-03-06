84a939678df37677569d3e226e8dee7119309816
unknown <aniruddhaghosh201545@gmail.com>
2025-03-07 01:14:41 +0530

    New Features Added : IP Address Info Search and minor code changes

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const maxmind = require('maxmind');
 const whoiser = require('whoiser');
 const port = process.env.PORT || 5000;
 app.use(express.json());
+
 app.get('/ipdata', async (req, res) => {
       try {
        
@@ -36,7 +37,8 @@ app.get('/ipdata', async (req, res) => {
       }
 });
 app.post('/search-whois', async(req, res) => {
-  try {
+  try 
+  {
     const { whoislookupsearch } = req.body;
     if(whoislookupsearch.length==0)
     {
@@ -51,7 +53,7 @@ app.post('/search-whois', async(req, res) => {
 } catch (error) {
     
     console.error('Error processing data:', error);
-    if(error.message.includes("Empty Search Field Error"))
+    if(error.message.includes("Please enter domain name or IP in the input search field"))
     {
       res.status(400 ).json({ error: 'Empty Search Field Error' });
     }
@@ -62,22 +64,84 @@ app.post('/search-whois', async(req, res) => {
    
 }
 });
+app.post('/ip-info-search', async(req, res) => {
+
+  try {
+    const { ipaddresssearch } =  req.body;
+    function isValidIP(ipaddress) 
+    {
+    
+        const ipv46_regex = /(?:^(?:25[0-5]|2[0-4]\d|1\d\d|[1-9]\d|\d)(?:\.(?:25[0-5]|2[0-4]\d|1\d\d|[1-9]\d|\d)){3}$)|(?:^(?:(?:[a-fA-F\d]{1,4}:){7}(?:[a-fA-F\d]{1,4}|:)|(?:[a-fA-F\d]{1,4}:){6}(?:(?:25[0-5]|2[0-4]\d|1\d\d|[1-9]\d|\d)(?:\\.(?:25[0-5]|2[0-4]\d|1\d\d|[1-9]\d|\d)){3}|:[a-fA-F\d]{1,4}|:)|(?:[a-fA-F\d]{1,4}:){5}(?::(?:25[0-5]|2[0-4]\d|1\d\d|[1-9]\d|\d)(?:\\.(?:25[0-5]|2[0-4]\d|1\d\d|[1-9]\d|\d)){3}|(?::[a-fA-F\d]{1,4}){1,2}|:)|(?:[a-fA-F\d]{1,4}:){4}(?:(?::[a-fA-F\d]{1,4}){0,1}:(?:25[0-5]|2[0-4]\d|1\d\d|[1-9]\d|\d)(?:\\.(?:25[0-5]|2[0-4]\d|1\d\d|[1-9]\d|\d)){3}|(?::[a-fA-F\d]{1,4}){1,3}|:)|(?:[a-fA-F\d]{1,4}:){3}(?:(?::[a-fA-F\d]{1,4}){0,2}:(?:25[0-5]|2[0-4]\d|1\d\d|[1-9]\d|\d)(?:\\.(?:25[0-5]|2[0-4]\d|1\d\d|[1-9]\d|\d)){3}|(?::[a-fA-F\d]{1,4}){1,4}|:)|(?:[a-fA-F\d]{1,4}:){2}(?:(?::[a-fA-F\d]{1,4}){0,3}:(?:25[0-5]|2[0-4]\d|1\d\d|[1-9]\d|\d)(?:\\.(?:25[0-5]|2[0-4]\d|1\d\d|[1-9]\d|\d)){3}|(?::[a-fA-F\d]{1,4}){1,5}|:)|(?:[a-fA-F\d]{1,4}:){1}(?:(?::[a-fA-F\d]{1,4}){0,4}:(?:25[0-5]|2[0-4]\d|1\d\d|[1-9]\d|\d)(?:\\.(?:25[0-5]|2[0-4]\d|1\d\d|[1-9]\d|\d)){3}|(?::[a-fA-F\d]{1,4}){1,6}|:)|(?::(?:(?::[a-fA-F\d]{1,4}){0,5}:(?:25[0-5]|2[0-4]\d|1\d\d|[1-9]\d|\d)(?:\\.(?:25[0-5]|2[0-4]\d|1\d\d|[1-9]\d|\d)){3}|(?::[a-fA-F\d]{1,4}){1,7}|:)))(?:%[0-9a-zA-Z]{1,})?$)/gm;  
+        return  ipv46_regex.test(ipaddress);
+    }
+   
+    function isPrivateIP(ipaddress) 
+    {
+        const privateIPv4Regex = /^(10\.\d+\.\d+\.\d+)$|^(192\.168\.\d+\.\d+)$|^(172\.(1[6-9]|2\d|3[0-1])\.\d+\.\d+)$|^(127\.\d+\.\d+\.\d+)$|^(169\.254\.\d+\.\d+)$/;
+        const privateIPv6Regex = /^(fe80::|fc00::)/i;
+        return privateIPv4Regex.test(ipaddress) || privateIPv6Regex.test(ipaddress);
+    }
+    if(!ipaddresssearch)
+    {
+         return res.status(400).json({error :"Please enter IP address in the input search field"});
+    }
+    if (!isValidIP(ipaddresssearch)) 
+    {
+        return res.status(400).json({ error: 'Invalid IP address format' });
+    }
+    if(isPrivateIP(ipaddresssearch)) 
+    {
+      return res.status(400).json({ error: 'It is a private IP address'});
+    }
+   
+
+        const ipAddress = ipaddresssearch;
+        const geolite2 = await import('geolite2-redist');
+        const reader = await geolite2.open(
+            'GeoLite2-City',
+            (dbPath) => maxmind.open(dbPath)
+        );
+     
+
+        const response = reader.get(ipAddress);
+        reader.close();
+        const ipDataInfo = {
+              ipaddress : ipAddress,
+              country: response.country.names.en,
+              city: response.city.names.en,
+              postalCode: response.postal.code,
+              timezone: response.location.time_zone,
+              lat: response.location.latitude,
+              lon: response.location.longitude
+      
+        }
+        res.json(ipDataInfo);
+    
+      } catch (error) {
+        console.error('Error occurred:', error);
+        res.status(500).json({ error: error.message });
+      }
+      
+
+    
+});
    
 app.use(express.static(path.join(__dirname, 'public')));
 
-const allowedRoutes = ['/home', '/ipsysinfo', '/whois', '/randompasswordgenerator'];
+const allowedRoutes = ['/home', '/ipsysinfo', '/whois', '/randompasswordgenerator', '/ipinfosearch'];
 
 app.get(allowedRoutes, (req, res) => {
 
   res.sendFile(path.join(__dirname, 'public', 'index.html'));
 
 });
+
 app.get('*', (req, res) => {
   // Redirect to the custom 404 page
   res.status(404).redirect('/404NotFound.html');
 });
 
-app.get('/404NotFound', (req, res) => {
+ app.get('/404NotFound', (req, res) => {
   res.status(404).sendFile(path.join(__dirname, 'public', '404NotFound.html'));
 });
 
---
+++ public/js/ipaddresssearch.js
@@ -0,0 +1,144 @@
+
+async function send_fetch_data_ipaddresssearch()
+{
+    let ipaddsearcherror_errorMsg = ["Please enter IP address in the input search field","Invalid IP address format", "It is a private IP address", "Couldn't complete this task due to a network error. Please try again later or at another time."];     
+    let ipaddresssearch = document.getElementById("ipaddresssearch").value;
+    let wait = document.getElementById('wait');
+    let ipaddressdivcenter = document.getElementsByClassName("ipaddressdivcenter");
+    let ipaddsearchinformation = document.getElementsByClassName("ipaddsearchinformation");
+    let ipaddressinfo = document.getElementsByClassName("ipaddressinfo");
+    let error_ipaddsearch= document.getElementsByClassName("error_ipaddsearch");
+    let error_ipaddsearch_messageText = document.getElementById("error_ipaddsearch_messageText");
+    if(ipaddsearchinformation[0].style.display==="block")
+    {
+            ipaddressdivcenter[0].style.display = "none";
+            ipaddsearchinformation[0].style.display = "none";
+    }
+    if(error_ipaddsearch[0].style.display==="block")
+    {
+                error_ipaddsearch[0].style.display = "none";
+                error_ipaddsearch_messageText.textContent = null;
+    }
+    if(ipaddresssearch.length===0)
+    {
+        error_ipaddsearch[0].style.display="block";
+        error_ipaddsearch_messageText.textContent = ipaddsearcherror_errorMsg[0];
+    }
+    else if (!isValidIP(ipaddresssearch)) 
+    {
+        error_ipaddsearch[0].style.display = "block";
+        error_ipaddsearch_messageText.textContent = ipaddsearcherror_errorMsg[1];
+ 
+    }
+    else if (isPrivateIP(ipaddresssearch)) 
+    {
+        error_ipaddsearch[0].style.display = "block";
+        error_ipaddsearch_messageText.textContent = ipaddsearcherror_errorMsg[2];
+    }
+    else
+    {
+        response = await fetch("/ip-info-search", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body : JSON.stringify({ipaddresssearch:ipaddresssearch})
+        })
+        .then(response => response.json())
+        .then(data => {
+            wait.style.display="none";
+            ipaddressdivcenter[0].style.display = "block";
+            ipaddsearchinformation[0].style.display = 'block';
+            let ipaddressinfofetch = [data.ipaddress, data.country, data.city, data.postalCode, data.timezone, data.lat, data.lon];
+            for(i=0;i<ipaddressinfo.length;i++)
+            {
+                
+    
+                if(ipaddressinfofetch[i]==undefined)
+                {
+                    ipaddressinfo[i].textContent = "Not Available";
+                   
+                }
+                else
+                {
+                    ipaddressinfo[i].textContent = ipaddressinfofetch[i];
+                 
+                }
+               
+            }
+        
+        })
+        .catch(error => {
+            error_ipaddsearch[0].style.display="block";
+            error_ipaddsearch_messageText.textContent = ipaddsearcherror_errorMsg[3];
+            console.error(error);
+        });
+        
+    }
+}
+function buttonclose()
+{   
+   
+    let error_ipaddsearch= document.getElementsByClassName("error_ipaddsearch");
+    let error_ipaddsearch_messageText = document.getElementById("error_ipaddsearch_messageText");
+    let ipaddressdivcenter = document.getElementsByClassName("ipaddressdivcenter");
+    let ipaddsearchinformation = document.getElementsByClassName("ipaddsearchinformation");
+    let ipaddressinfo = document.getElementsByClassName("ipaddressinfo");
+    if( error_ipaddsearch[0].style.display==="block")
+    {
+        error_ipaddsearch[0].style.display = "none";
+        error_ipaddsearch_messageText.textContent =  null;
+    }
+    else if(ipaddsearchinformation[0].style.display==="block")
+    {
+        ipaddressdivcenter[0].style.display = "none";
+        ipaddsearchinformation[0].style.display="none";
+        for(i=0;i<ipaddressinfo.length;i++)
+        {
+            ipaddressinfo[i].textContent = null;
+        }
+    }
+}
+function revisiting_the_ipinfosearcb_search_page()
+{
+    let ipaddresssearch = document.getElementById("ipaddresssearch");
+    if(ipaddresssearch.value.length>0)
+    {
+        ipaddresssearch.value = null;
+    }
+    buttonclose();
+}
+function isValidIP(ipaddress) 
+{
+
+    const ipv46_regex = /(?:^(?:25[0-5]|2[0-4]\d|1\d\d|[1-9]\d|\d)(?:\.(?:25[0-5]|2[0-4]\d|1\d\d|[1-9]\d|\d)){3}$)|(?:^(?:(?:[a-fA-F\d]{1,4}:){7}(?:[a-fA-F\d]{1,4}|:)|(?:[a-fA-F\d]{1,4}:){6}(?:(?:25[0-5]|2[0-4]\d|1\d\d|[1-9]\d|\d)(?:\\.(?:25[0-5]|2[0-4]\d|1\d\d|[1-9]\d|\d)){3}|:[a-fA-F\d]{1,4}|:)|(?:[a-fA-F\d]{1,4}:){5}(?::(?:25[0-5]|2[0-4]\d|1\d\d|[1-9]\d|\d)(?:\\.(?:25[0-5]|2[0-4]\d|1\d\d|[1-9]\d|\d)){3}|(?::[a-fA-F\d]{1,4}){1,2}|:)|(?:[a-fA-F\d]{1,4}:){4}(?:(?::[a-fA-F\d]{1,4}){0,1}:(?:25[0-5]|2[0-4]\d|1\d\d|[1-9]\d|\d)(?:\\.(?:25[0-5]|2[0-4]\d|1\d\d|[1-9]\d|\d)){3}|(?::[a-fA-F\d]{1,4}){1,3}|:)|(?:[a-fA-F\d]{1,4}:){3}(?:(?::[a-fA-F\d]{1,4}){0,2}:(?:25[0-5]|2[0-4]\d|1\d\d|[1-9]\d|\d)(?:\\.(?:25[0-5]|2[0-4]\d|1\d\d|[1-9]\d|\d)){3}|(?::[a-fA-F\d]{1,4}){1,4}|:)|(?:[a-fA-F\d]{1,4}:){2}(?:(?::[a-fA-F\d]{1,4}){0,3}:(?:25[0-5]|2[0-4]\d|1\d\d|[1-9]\d|\d)(?:\\.(?:25[0-5]|2[0-4]\d|1\d\d|[1-9]\d|\d)){3}|(?::[a-fA-F\d]{1,4}){1,5}|:)|(?:[a-fA-F\d]{1,4}:){1}(?:(?::[a-fA-F\d]{1,4}){0,4}:(?:25[0-5]|2[0-4]\d|1\d\d|[1-9]\d|\d)(?:\\.(?:25[0-5]|2[0-4]\d|1\d\d|[1-9]\d|\d)){3}|(?::[a-fA-F\d]{1,4}){1,6}|:)|(?::(?:(?::[a-fA-F\d]{1,4}){0,5}:(?:25[0-5]|2[0-4]\d|1\d\d|[1-9]\d|\d)(?:\\.(?:25[0-5]|2[0-4]\d|1\d\d|[1-9]\d|\d)){3}|(?::[a-fA-F\d]{1,4}){1,7}|:)))(?:%[0-9a-zA-Z]{1,})?$)/gm;  
+
+    return  ipv46_regex.test(ipaddress);
+}
+function isPrivateIP(ipaddress) 
+{
+    const privateIPv4Regex = /^(10\.\d+\.\d+\.\d+)$|^(192\.168\.\d+\.\d+)$|^(172\.(1[6-9]|2\d|3[0-1])\.\d+\.\d+)$|^(127\.\d+\.\d+\.\d+)$|^(169\.254\.\d+\.\d+)$/;
+    const privateIPv6Regex = /^(fe80::|fc00::)/i;
+    return privateIPv4Regex.test(ipaddress) || privateIPv6Regex.test(ipaddress);
+}
+function handleformSubmission()
+{
+    document.getElementById('ipsearch').addEventListener('submit', function(event) {
+            event.preventDefault(); // Prevent the default form submission
+            send_fetch_data_ipaddresssearch();
+        });
+            function submitOnEnter(event) {
+            if (event.which === 13) {
+                if (!event.repeat) {
+                    const newEvent = new Event("submit", {cancelable: true});
+                    event.target.form.dispatchEvent(newEvent);
+                    event.target.form.focus(newEvent);
+
+
+                }
+
+                event.preventDefault(); 
+            }
+        }
+      document.getElementById("ipaddresssearch").addEventListener("keydown", submitOnEnter);
+}
